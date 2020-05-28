df0b3edb63791a45a71249ecdf117d79c417c612
Tamás Szabó <tamas@szaboo.com>
2020-05-28 16:16:17 +0200

    404 design update

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,22 @@
-import React from "react"
+import React from 'react'
+import { css } from 'styled-components'
+
+import { Layout } from '../components'
 
 const NotFoundPage = () => (
-  <div>
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </div>
+  <Layout>
+    <div
+      css={css`
+        text-align: center;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+      `}
+    >
+      <h1>NOT FOUND</h1>
+      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    </div>
+  </Layout>
 )
 
 export default NotFoundPage
